8a4772e099ce37eade6fcdfcc3abfcd04a7b0d29
Maxim Fainberg <maksym.fainberg@mobenga.com>
2021-12-17 00:43:41 +0100

    Day 3 - part 2

--- clojure/2021/src/adv/t03.clj
+++ clojure/2021/src/adv/t03.clj
@@ -24,7 +24,7 @@
 
 (defn gamma [xs]
   (let [half (/ (count xs) 2)]
-    (->> input
+    (->> xs
          (count-ones)
          (map (fn [cnt] (if (> cnt half) 1 0))))))
 
@@ -38,4 +38,40 @@
 (defn f []
   (solve input))
 
+; Part 2
+(defn bit-criteria [xs idx most-common?]
+  (let [half (/ (count xs) 2)]
+    (->> xs
+         (map #(% idx))
+         (reduce +)
+         (#(if (or (and most-common? (>= % half))
+                   (and (not most-common?) (< % half)))
+              1
+              0)))))
 
+(defn filter-for-rating [xs idx most-common?]
+  (let [b (bit-criteria xs idx most-common?)]
+    (filter (fn [ys] (= (ys idx) b))
+            xs)))
+
+(defn find-rating [xs idx most-common?]
+  (prn 222 idx xs)
+  (if (< (count xs) 2)
+     (first xs)
+     (recur (filter-for-rating xs idx most-common?)
+            (inc idx)
+            most-common?)))  
+
+(defn find-oxygen-generator-rating [xs] 
+  (find-rating xs 0 true))
+
+(defn find-co2-scrubber-rating [xs]
+  (find-rating xs 0 false))
+
+(defn solve2 [xs]
+  (->> [find-oxygen-generator-rating find-co2-scrubber-rating]
+       (map #(% xs))
+       (map binary-array-to-number)
+       (reduce *)))
+
+(defn f2 [] (solve2 input))
