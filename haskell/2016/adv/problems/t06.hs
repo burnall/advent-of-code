ffeac519704ef7c552d6d44fd84e72cb9a16409d
Maxim F <maximfainberg@gmail.com>
2025-10-25 23:14:16 +0200

    Day 6

---
+++ haskell/2016/adv/problems/t06.hs
@@ -0,0 +1,15 @@
+-- stack script --resolver lts-21.25 --package split  --package containers
+import Data.Function ((&))
+import Data.List (group, sort, maximumBy, head, transpose, minimumBy)
+import Data.Ord (comparing)
+
+main :: IO ()
+main = do
+  content <- readFile "../data/t06.txt"
+  print $ task1 $ lines content
+
+task1 :: [String] -> String 
+task1 = map mostFrequent . transpose
+
+mostFrequent :: String -> Char
+mostFrequent = head . minimumBy (comparing length) . group . sort
