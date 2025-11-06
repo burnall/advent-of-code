28cdf12f2ea5fccab12d8d4e248c1cd03cb2db54
Maxim F <maximfainberg@gmail.com>
2025-11-06 16:06:35 +0100

    Day 10 improve

--- haskell/2016/adv/problems/t10.hs
+++ haskell/2016/adv/problems/t10.hs
@@ -1,4 +1,4 @@
--- stack script problems/t09.hs --resolver lts-22.18 --package containers --package regex-tdfa --package regex-base --package array
+-- stack script problems/t10.hs --resolver lts-22.18 --package containers --package regex-tdfa --package regex-base --package array
 
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -19,19 +19,12 @@ type PinState = Map String [Int]
 main :: IO ()
 main = do
   content <- readFile "../data/t10.txt"
-  let 
-    instrs = map parseInstr $ lines content
-    finalState = run instrs
-    r = Map.filter (\v -> v == [17, 61]) finalState
-    --initState = getInitialState instrs
-    --completePins = Map.keys $ Map.filter (\vs -> length vs == 2) initState
-    --moves = Map.fromList [(from i, i) | i@Move{} <- instrs]
-  --print moves
+  let instrs = map parseInstr $ lines content
+      finalState = run instrs
+      r = Map.filter (\v -> v == [17, 61]) finalState
+      r2 = foldl1 (*) $ concatMap (\k -> finalState Map.! k) ["output0", "output1", "output2"]
   print r
-
--- print $ parseInstr "value 3 goes to bot 1"
--- print $ ff "bot 49 gives low to bot 118 and high to bot 182"
--- print $ parseInstr "bot 49 gives low to bot 118 and high to bot 182"
+  print r2
 
 parseInstr :: String -> Instr
 parseInstr s =
@@ -43,31 +36,35 @@ parseInstr s =
     pattern = "value ([0-9]+) goes to bot ([0-9]+)|bot ([0-9]+) gives low to (bot|output) ([0-9]+) and high to (bot|output) ([0-9]+)"
 
 run :: [Instr] -> PinState
-run instrs = runPins initState completePins
+run instrs = applyMoves moves initState
   where
     initState = getInitialState instrs
-    completePins = Map.keys $ Map.filter (\vs -> length vs == 2) initState
-    moves = Map.fromList [(from i, i) | i@Move{} <- instrs]
-    runPins :: PinState -> [String] -> PinState
-    runPins state [] = state
-    runPins state activePins =
-      let (newState, newPins) = foldl runPin (state, []) activePins
-        in runPins newState newPins
-    runPin :: (PinState, [String]) -> String -> (PinState, [String]) 
-    runPin (state, pins) pin =
-      let [low, high] = state Map.! pin
-          Move {lowTo, highTo} = moves Map.! pin
-          newState = resolveState (resolveState state lowTo low) highTo high
-          has2 k = length (newState Map.! k) == 2
-          newPins = if has2 lowTo then lowTo : pins else pins
-          newPins2 = if has2 highTo then highTo : newPins else newPins
-       in (newState, newPins2)
+    moves = Map.fromList [(from i, i) | i@Move {} <- instrs]
+
+applyMoves :: Map String Instr -> PinState -> PinState
+applyMoves moves state = go state readyPins
+  where
+    readyPins = Map.keys $ Map.filter (\vs -> length vs == 2) state
+    go state [] = state
+    go state activePins =
+      let (state', pins) = foldl (applyMove moves) (state, []) activePins
+       in go state' pins
+
+applyMove :: Map String Instr -> (PinState, [String]) -> String -> (PinState, [String])
+applyMove moves (state, pins) pin = (newState, newPins')
+  where
+    [low, high] = state Map.! pin
+    Move {lowTo, highTo} = moves Map.! pin
+    newState = resolveState (resolveState state lowTo low) highTo high
+    has2 k = length (newState Map.! k) == 2
+    newPins = if has2 lowTo then lowTo : pins else pins
+    newPins' = if has2 highTo then highTo : newPins else newPins
 
 getInitialState :: [Instr] -> PinState
-getInitialState instrs = foldl step Map.empty inits
+getInitialState instrs = foldl applyInit Map.empty inits
   where
     inits = [i | i@(Init _ _) <- instrs]
-    step agg init = resolveState agg (uid init) (val init)
+    applyInit state init = resolveState state (uid init) (val init)
 
 resolveState :: PinState -> String -> Int -> PinState
 resolveState state key value = Map.insertWith upd key [value] state
