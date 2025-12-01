801ff5d229682f4efb80c970a636d53030b1b6d8
Maxim F <maximfainberg@gmail.com>
2025-12-01 15:36:13 +0100

    Day 1 part 2 java

--- java/adv25/src/main/java/com/example/Task01.java
+++ java/adv25/src/main/java/com/example/Task01.java
@@ -12,15 +12,29 @@ public class Task01 {
     public static void main(String[] args) throws Exception {
         List<String> lines = Files.readAllLines(Paths.get("src/main/resources/t01.txt"));
         List<Cmd> cmds = parseCmds(lines);
-        System.out.println(solve1(cmds));
+        System.out.println(solve2(cmds));
     }
 
     private static long solve1(List<Cmd> cmds) {
-        AtomicInteger sum = new AtomicInteger(50);
+        AtomicInteger pos = new AtomicInteger(50);
         return cmds.stream().map(cmd -> cmd.clicks() * (cmd.clockwise() ? 1 : -1))
-                .map(sum::addAndGet)
+                .map(pos::addAndGet)
                 .filter(i -> i % 100 == 0)
                 .count();
+    }
+
+    private static long solve2(List<Cmd> cmds) {
+        AtomicInteger pos = new AtomicInteger(50);
+
+        return cmds.stream().map(cmd -> cmd.clicks() * (cmd.clockwise() ? 1 : -1))
+                .map(i-> {
+                    int p = pos.get();
+                    pos.set(mod100(p + i));
+                    System.out.printf("%d %d %d\n", p, i, turns(p, i));
+                    return turns(p, i);
+                })
+                .mapToLong(Integer::intValue)
+                .sum();
     }
 
     private static List<Cmd> parseCmds(List<String> lines) {
@@ -28,5 +42,15 @@ public class Task01 {
                 .map(line -> new Cmd('R' == line.charAt(0),
                         Integer.parseInt(line.substring(1))))
                 .toList();
+    }
+
+    private static int turns(int start, int clicks) {
+        return clicks >= 0
+                ? (start + clicks) / 100
+                : ((start == 0 ? 0 : 100 - start) - clicks) / 100;
+    }
+
+    private static int mod100(int a) {
+        return (100 + a % 100) % 100;
     }
 }
