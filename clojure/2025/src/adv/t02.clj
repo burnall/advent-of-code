06978bd5c87c9185c6cf5d58e243674e5e81b720
Maxim F <maximfainberg@gmail.com>
2025-12-06 21:15:29 +0100

    Day 2 clojure

---
+++ clojure/2025/src/adv/t02.clj
@@ -0,0 +1,30 @@
+(ns adv.t02
+  (:require [adv.util :refer [split]]))
+
+(defn parse-range [s]
+  (->> (split s #"-")
+       (map parse-long)
+       ((fn [[a b]] (range a (inc b))))))
+
+(def input
+  (as-> "resources/t02.txt" $
+    (slurp $)
+    (split $ #",")
+    (map parse-range $)))
+
+(defn invalid2? [xs]
+  (some #(apply = (partition-all % xs))
+        (range 1 (inc (quot (count xs) 2)))))
+
+(defn invalid? [xs]
+  (let [cnt (count xs)]
+    (when (even? cnt)
+      (apply = (partition-all (quot (count xs) 2) xs)))))
+
+(defn task1 [rs]
+  (->> rs
+       (mapcat (partial filter (comp invalid? str)))
+       ));(apply +)))
+
+(task1 [(range 1 100)])
+
